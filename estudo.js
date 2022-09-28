ec8e06ac7c4e43fc63fdf17077f13b7a1e438a98
gabinovic <106194472+gabinovic@users.noreply.github.com>
2022-09-28 16:56:19 -0300

    add tabuada

---
+++ estudo.js
@@ -0,0 +1,18 @@
+// alert("Oi! Eu sou o Alert");
+// document.write("Estou na página.");
+// console.log("Eu estou no console.");
+let tabuada = 2;
+function alo(){
+    document.write(){
+        document.write(tabuada + " x 1 = "(tabuada*1)+"<br>");
+        document.write(tabuada + " x 2 = "(tabuada*1)+"<br>");
+        document.write(tabuada + " x 3 = "(tabuada*1)+"<br>");
+        document.write(tabuada + " x 4 = "(tabuada*1)+"<br>");
+        document.write(tabuada + " x 5 = "(tabuada*1)+"<br>");
+        document.write(tabuada + " x 6 = "(tabuada*1)+"<br>");
+        document.write(tabuada + " x 7 = "(tabuada*1)+"<br>");
+        document.write(tabuada + " x 8 = "(tabuada*1)+"<br>");
+        document.write(tabuada + " x 9 = "(tabuada*1)+"<br>");
+        document.write(tabuada + " x 10 = "(tabuada*1));
+    }
+}
